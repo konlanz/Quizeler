8e3d658b6254542407c1bbceb8c1dfe91d852f4d
konlanmikpekoah.km@outlook.com <konlanmikpekoah.km@outlook.com>
2021-01-12 15:58:04 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(QuizPage());
+}
+
+class QuizPage extends StatelessWidget {
+  const QuizPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: QuizLer(),
+        )),
+      ),
+    );
+  }
+}
+
+class QuizLer extends StatefulWidget {
+  QuizLer({Key key}) : super(key: key);
+
+  @override
+  _QuizLerState createState() => _QuizLerState();
+}
+
+class _QuizLerState extends State<QuizLer> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text('this is where the questions will go',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+         Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
